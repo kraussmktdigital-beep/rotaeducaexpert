e0337ba0a84eccf79f3ba8019f6bf25b3b6f8d1b
KRAÜSS <kraussmktdigital@gmail.com>
2026-04-19 06:57:55 -0300

    first deploy

---
+++ firebase.js
@@ -0,0 +1,8 @@
+﻿const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "rotaexpertlite.firebaseapp.com",
+  projectId: "rotaexpertlite",
+  storageBucket: "rotaexpertlite.appspot.com",
+  messagingSenderId: "1011801483543",
+  appId: "1:1011801483543:web:79a37aef4cf6851b4b6f52"
+};
